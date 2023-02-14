b5d1c4434190526beb7938b99848729829a72fb6
samdev <samstore637@gmail.com>
2023-02-14 14:04:40 +0300

    added some wording

--- src/pages/about/info.js
+++ src/pages/about/info.js
@@ -1,4 +1,4 @@
-export const primaryDefination = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, nibh tincidunt condimentum sollicitudin, diam sem interdum massa, eu maximus nunc mauris at ante. Etiam malesuada non nulla malesuada malesuada."
+export const primaryDefination = "Tandem skydiving is a great option for first-time jumpers or those who want to experience the thrill of skydiving without the commitment of completing a solo jump. During a tandem jump, you will be securely attached to one of our experienced instructors throughout the entire jump, allowing you to relax and enjoy the experience, including the free fall. You will be able to take in the breathtaking views of Kenya while your instructor handles all the technical aspects of the jump. Tandem skydiving is a safe and exciting way to experience the thrill of free falling from an airplane, and is perfect for those who want to make memories that will last a lifetime. Book your tandem skydiving experience today and discover the rush of adrenaline that comes with leaping out of an airplane and free falling through the sky!"
 export const definationParagraphs = [
 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, nibh tincidunt condimentum sollicitudin, diam sem interdum massa, eu maximus nunc mauris at ante. Etiam malesuada non nulla malesuada malesuada. Phasellus pharetra massa ligula, nec mattis turpis dictum nec. Aenean sollicitudin et erat quis scelerisque. Nullam pellentesque neque lobortis fermentum sodales. Sed quis augue id sem ullamcorper rutrum. Quisque posuere iaculis interdum. Nulla varius tincidunt lacus sit amet pharetra. Cras elementum magna non nisl dignissim commodo. Duis et tortor ac justo mollis imperdiet.",
 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, nibh tincidunt condimentum sollicitudin, diam sem interdum massa, eu maximus nunc mauris at ante. Etiam malesuada non nulla malesuada malesuada. Phasellus pharetra massa ligula, nec mattis turpis dictum nec. Aenean sollicitudin et erat quis scelerisque. Nullam pellentesque neque lobortis fermentum sodales. Sed quis augue id sem ullamcorper rutrum. Quisque posuere iaculis interdum. Nulla varius tincidunt lacus sit amet pharetra. Cras elementum magna non nisl dignissim commodo. Duis et tortor ac justo mollis imperdiet.",
@@ -8,11 +8,59 @@ export const jumpCardContent = [
 	{
 		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1673047336/skydive%20rhino/images/kamil-pietrzak-H22w-tq0SeQ-unsplash_cn6hbi.jpg",
 		title: "Tandem Jump",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, nibh tincidunt condimentum sollicitudin, diam sem interdum massa, eu maximus nunc mauris at ante. Etiam malesuada non nulla malesuada malesuada. Phasellus pharetra massa ligula, nec mattis turpis dictum nec. Aenean sollicitudin et erat quis scelerisque. Nullam pellentesque neque lobortis fermentum sodales. Sed quis augue id sem ullamcorper rutrum. Quisque posuere iaculis interdum. Nulla varius tincidunt lacus sit amet pharetra. Cras elementum magna non nisl dignissim commodo. Duis et tortor ac justo mollis imperdiet.",
+		text: "Tandem skydiving is a great option for first-time jumpers or those who want to experience the thrill of skydiving without the commitment of completing a solo jump. During a tandem jump, you will be securely attached to one of our experienced instructors throughout the entire jump, allowing you to relax and enjoy the experience. You will be able to take in the breathtaking views of Kenya while your instructor handles all the technical aspects of the jump. Tandem skydiving is a safe and exciting way to experience the thrill of skydiving, and is perfect for those who want to make memories that will last a lifetime. Book your tandem skydiving experience today and discover the rush of adrenaline that comes with leaping out of an airplane and flying through the sky!",
 	},
 	{
 		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1673047336/skydive%20rhino/images/kamil-pietrzak-H22w-tq0SeQ-unsplash_cn6hbi.jpg",
-		title: "Jump School",
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, nibh tincidunt condimentum sollicitudin, diam sem interdum massa, eu maximus nunc mauris at ante. Etiam malesuada non nulla malesuada malesuada. Phasellus pharetra massa ligula, nec mattis turpis dictum nec. Aenean sollicitudin et erat quis scelerisque. Nullam pellentesque neque lobortis fermentum sodales. Sed quis augue id sem ullamcorper rutrum. Quisque posuere iaculis interdum. Nulla varius tincidunt lacus sit amet pharetra. Cras elementum magna non nisl dignissim commodo. Duis et tortor ac justo mollis imperdiet.",
+		title: "Photography and videography",
+		text: "Tandem skydiving is not only an exciting and thrilling experience, but it's also a great opportunity to capture beautiful and unique memories. That's why at Sky Dive Rhino Kenya, we offer professional photography and videography services for our tandem jumpers. Our experienced photographers and videographers will capture every moment of your jump, from the initial leap out of the plane to the peaceful descent under the parachute. You'll be able to relive the experience and share it with your loved ones. Our professional photography and videography package allows you to have a tangible memory of your skydiving experience, and can be a great way to commemorate a special occasion or milestone. So don't hesitate, book your tandem skydiving experience today and let us capture the unforgettable moments for you.",
+	},
+	{
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1673047336/skydive%20rhino/images/kamil-pietrzak-H22w-tq0SeQ-unsplash_cn6hbi.jpg",
+		title: "Helicopter skydiving",
+		text: "We offer the exciting and exhilarating activity of helicopter skydiving for those looking for an even more thrilling skydiving experience. During a helicopter skydiving jump, participants will jump out of a helicopter at high altitude and experience the rush of free falling from a greater height than a traditional airplane jump. Our experienced instructors will guide you through the proper techniques and safety protocols necessary for a successful helicopter skydiving jump. They will also provide training and coaching to help you master the skills necessary to jump safely and efficiently from a helicopter. The stunning views and the feeling of falling from a greater height make helicopter skydiving an unforgettable experience. At Sky Dive Rhino Kenya, we take safety seriously, and only experienced jumpers are allowed to participate in helicopter skydiving. Book your helicopter skydiving experience today and discover the thrill of falling from the sky from even greater heights.",
+	},
+]
+
+export const moreContent = [
+	{
+		title: "Why Tandem?",
+		paragraphs: [
+			`Tandem skydiving is a great option for first-time jumpers or those who want to experience the thrill of skydiving without the commitment of completing a solo jump. During a tandem jump, you will be securely attached to one of our experienced instructors throughout the entire jump, allowing you to relax and enjoy the experience, including the free fall. You will be able to take in the breathtaking views of Kenya while your instructor handles all the technical aspects of the jump`,
+			`Tandem skydiving is a safe and exciting way to experience the thrill of free falling from an airplane, and is perfect for those who want to make memories that will last a lifetime. Book your tandem skydiving experience today and discover the rush of adrenaline that comes with leaping out of an airplane and free falling through the sky!`,
+		]
+	},
+	{
+		title: "Photography and videography",
+		paragraphs: [
+			`Tandem skydiving is not only an exciting and thrilling experience, but it's also a great opportunity to capture beautiful and unique memories. That's why at Sky Dive Rhino Kenya, we offer professional photography and videography services for our tandem jumpers. Our experienced photographers and videographers will capture every moment of your jump, from the initial leap out of the plane to the peaceful descent under the parachute. You'll be able to relive the experience and share it with your loved ones.`,
+			`Our professional photography and videography package allows you to have a tangible memory of your skydiving experience, and can be a great way to commemorate a special occasion or milestone. So don't hesitate, book your tandem skydiving experience today and let us capture the unforgettable moments for you.`,
+		]
+	},
+	{
+		title: "Free Fall",
+		paragraphs: [
+			`At Sky Dive Rhino Kenya, we offer the opportunity for thrill-seekers to take their skydiving experience to the next level with our group free fall skydiving. Also known as formation skydiving, this activity allows a group of individuals to jump out of the plane together and experience the exhilaration of free falling in formation.`,
+			`Our experienced instructors will guide you and your group through the proper techniques and hand signals needed to execute the formation, ensuring a safe and enjoyable experience for all participants. This activity is perfect for groups of friends, families, or even corporate team building events.`,
+			`The rush of falling through the sky in formation with your loved ones or colleagues is an experience that is truly unforgettable. Not only that, but our professional photographers and videographers will be on hand to capture the beautiful and unique memories of your group free fall skydiving experience.`,
+			`Book your group free fall skydiving experience today and discover the thrill of falling through the sky in formation with your loved ones or colleagues. It's a perfect opportunity to make memories that will last a lifetime.`,
+		]
+	},
+	{
+		title: "Wing-suit Skydiving",
+		paragraphs: [
+			`At Sky Dive Rhino Kenya, we offer the exciting and exhilarating activity of wing suit skydiving for the experienced skydivers looking for a unique and thrilling experience.`,
+			`During a wing suit skydiving jump, participants will jump out of the plane wearing a special suit that allows them to fly through the air like a bird. This activity requires a high level of skill and experience, and is not recommended for first-time skydivers.`,
+			`Our experienced instructors will guide you through the proper techniques and safety protocols necessary for a successful wing suit skydiving jump. They will also provide training and coaching to help you master the skills necessary to fly through the air safely and efficiently.`,
+			`The stunning views and the feeling of flying like a bird make wing suit skydiving an unforgettable experience. At Sky Dive Rhino Kenya, we take safety seriously, and only experienced jumpers are allowed to participate in wing suit skydiving. Book your wing suit skydiving experience today and discover the thrill of flying through the air like a bird.`,
+		]
+	},
+	{
+		title: "Night Skydiving",
+		paragraphs: [
+			`At Sky Dive Rhino Kenya, we offer the unique and exciting experience of night skydiving for thrill-seekers looking for something different. Night skydiving is a once-in-a-lifetime experience that offers a unique perspective on the world around us, and is not something that you can find anywhere else.`,
+			`During a night skydiving jump, participants will jump out of the plane under the stars, and experience the thrill of free falling in the dark. Our experienced instructors will guide you through the proper techniques and safety protocols necessary for a successful night skydiving jump. They will also provide training and coaching to help you master the skills necessary to jump safely and efficiently at night.`,
+			`The stunning views of the night sky and the feeling of falling through the darkness make night skydiving an unforgettable experience. At Sky Dive Rhino Kenya, we take safety seriously, and only experienced jumpers are allowed to participate in night skydiving. Book your night skydiving experience today and discover the thrill of falling through the sky under the stars.`,
+		]
 	},
 ]
